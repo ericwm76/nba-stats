2568840e619957d6887b77dd08dcfa62ec08c2a2
Eric Meldrum <eric.meldrum@gmail.com>
2019-11-02 22:35:45 -0600

    Fetch data for individual player season averages on click of View Stats button

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,10 +1,25 @@
 const baseUrl = 'https://www.balldontlie.io/api/v1'
 
 export const searchByName = async (firstName, lastName) => {
-  let response = await fetch(`${baseUrl}/players/?search=${firstName}%20${lastName}&per_page=100`)
+  let response = await fetch(`${baseUrl}/players/?search=${firstName}%20${lastName}&per_page=100`);
+
   if(!response.ok) {
-    
+
   }
+
   let data = await response.json();
+
   return data.data
+}
+
+export const getPlayerSeasonAvgs = async (id) => {
+  let response = await fetch(`${baseUrl}/season_averages?player_ids[]=${id}`);
+
+  if(!response.ok) {
+
+  }
+
+  let data = await response.json();
+
+  return data.data;
 }
--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { getPlayerSeasonAvgs } from '../../apiCalls/apiCalls'
 
 export const Card = ({ player }) => {
+  const getStats = async (e, id) => {
+    e.preventDefault();
+    let avgs = await getPlayerSeasonAvgs(id);
+    console.log(avgs)
+    return avgs;
+  } 
+
   return (
     <div>
       <h3>Name: {player.first_name} {player.last_name}</h3>
@@ -9,7 +17,7 @@ export const Card = ({ player }) => {
       <h3>Weight: {player.weight_pounds} lbs</h3>
       <h3>Position: {player.position}</h3>
       <div className="card-footer">
-        <button>View Stats</button>
+        <button onClick={(e) => getStats(e, player.id)}>View Stats</button>
         <button>X</button>
       </div>
     </div>
