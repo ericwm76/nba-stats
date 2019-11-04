5de9f0555c7f221296812b7bfd0081111e92125f
Eric Meldrum <eric.meldrum@gmail.com>
2019-11-03 18:37:29 -0700

    Cause chart to render with proper labels and datasets on componentDidMount in Chart component, which renders on click of Compare Players button

--- src/containers/Chart/Chart.js
+++ src/containers/Chart/Chart.js
@@ -28,51 +28,64 @@ export class Chart extends Component {
       labels.push("Game " + (i + 1))
     }
 
+    console.log('labels: ', labels)
     this.setState({ labels: labels })
   }
 
   createDatasets() {
     const {stats, players} = this.props;
+    let datasets = [];
 
+    stats.forEach(stat => {
+      console.log(stat);
+      let id = parseInt(Object.keys(stat));
+      let player = players.find(player => player.id === id);
+      let playerName = player.first_name + ' ' + player.last_name;
+      let points = stat[id].map(game => game.pts);
+
+      datasets.push({
+        label: playerName,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: points
+      })
+    })
+
+    console.log('datasets: ', datasets)
+    this.setState({ datasets: datasets });
   }
 
   componentDidMount() {
     this.createLabels();
+    this.createDatasets();
   }
 
   render() {
-    const { labels } = this.state
+    const { labels, datasets } = this.state
 
     let data = {
       labels: labels.length > 10 ? labels.slice(-10) : labels,
-      datasets: [
-        {
-          label: 'My First dataset',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
+      datasets: datasets
     }
 
     return (
       <div>
-        <h2>Line Example</h2>
+        <h2>Points Per Game - Last 10 Games</h2>
         <Line data={data} />
       </div>
     );
